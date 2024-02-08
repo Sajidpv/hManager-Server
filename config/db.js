89c6b55926a2980d8c3e5c0ffe6886606fe7f5a0
SAJID PV <125041012+Sajidpv@users.noreply.github.com>
2024-02-08 11:07:30 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 // Connect to mongoose
 //const connection=mongoose.createConnection("mongodb://localhost:27017/hManager").on('open',()=>{
-const connection=mongoose.createConnection("mongodb+srv://hmanage:hManage@hmanager.gmyn7uu.mongodb.net/hmanager").on('open',()=>{
+const connection=mongoose.createConnection("db_url").on('open',()=>{
     console.log("Connected to db");
 }).on('error',()=>{
     console.log("Faild to Connected db");
